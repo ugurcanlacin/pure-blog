260e67caf862137e579608be495b23fddfa5fc25
ugurcanlacin <lacinugur@hotmail.com.tr>
2015-09-25 14:38:01 +0200

    Blocked login page after authenticated.

--- src/main/java/pureblog/controller/admin/AdminController.java
+++ src/main/java/pureblog/controller/admin/AdminController.java
@@ -1,16 +1,36 @@
 package pureblog.controller.admin;
 
+import java.util.Collection;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 	@RequestMapping("/login")
 	public ModelAndView getLoginPage(){
-		return new ModelAndView("login");
+		Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>)
+				  SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+		boolean hasRole = false;
+		for (GrantedAuthority authority : authorities) {
+		    hasRole = authority.getAuthority().equals("ADMIN");
+		    if (hasRole) 
+		    	break;
+		}
+		if(hasRole)
+			return new ModelAndView("admin/dashboard");
+		else
+			return new ModelAndView("login");
 	}
-	@RequestMapping("/admin/dashboard")
+	
+	@RequestMapping("/dashboard")
 	public ModelAndView getDashboardPage(){
 		return new ModelAndView("admin/dashboard");
 	}
